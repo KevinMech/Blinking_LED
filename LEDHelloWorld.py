303518e0fd1f8113cbb63816d0ed246a7c420ddc
Kevin <kevin.mellachew@gmail.com>
2017-01-08 16:47:47 -0500

    created script to blink the LED on GPIO 7

---
+++ LEDHelloWorld.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+for x in range(1, 11):
+    GPIO.output(7, True)
+    time.sleep(1)
+    GPIO.output(7, False)
+    time.sleep(1)
+
+GPIO.cleanup()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LED_Hello_World
 A script written in python for the raspberry pi that blinks an LED light on and off
+
+Uses GPIO pin 7 as the output for the LED
