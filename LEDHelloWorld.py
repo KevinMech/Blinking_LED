f68b07fe5dbd7e3aef70df04055f785a190af490
Kevin <kevin.mellachew@gmail.com>
2017-01-08 17:47:41 -0500

    Added an exception in order to allow the pi to reset pins if exited prematurely

--- LEDHelloWorld.py
+++ LEDHelloWorld.py
@@ -4,10 +4,12 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 
-for x in range(1, 11):
-    GPIO.output(7, True)
-    time.sleep(1)
-    GPIO.output(7, False)
-    time.sleep(1)
-
-GPIO.cleanup()
+try:
+    for x in range(1, 11):
+        GPIO.output(7, True)
+        time.sleep(1)
+        GPIO.output(7, False)
+        time.sleep(1)
+    GPIO.cleanup()
+except KeyboardInterrupt:
+    GPIO.cleanup()
